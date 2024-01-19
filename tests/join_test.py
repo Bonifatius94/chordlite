23a0787323cb7a288d6cd5ef6977df6a10616487
Marco Tröster <marco@troester-gmbh.de>
2024-01-19 12:17:53 +0100

    refactor, cleanup

--- tests/join_test.py
+++ tests/join_test.py
@@ -5,8 +5,9 @@ def test_can_init_two_node_network():
     n1 = ChordNode(ChordKey(0, 1024))
     n2 = ChordNode(ChordKey(100, 1024))
 
-    n1.initiate_join(n1)
-    n2.initiate_join(n1)
+    bootstrap = n1
+    n1.initiate_join(bootstrap)
+    n2.initiate_join(bootstrap)
 
     assert n1.successor == n2 and n1.predecessor == n2
     assert n2.successor == n1 and n2.predecessor == n1
@@ -19,9 +20,10 @@ def test_can_init_three_node_network():
     n2 = ChordNode(ChordKey(100, 1024))
     n3 = ChordNode(ChordKey(200, 1024))
 
-    n1.initiate_join(n1)
-    n2.initiate_join(n1)
-    n3.initiate_join(n1)
+    bootstrap = n1
+    n1.initiate_join(bootstrap)
+    n2.initiate_join(bootstrap)
+    n3.initiate_join(bootstrap)
 
     assert n1.successor == n2
     assert n2.successor == n3
@@ -33,23 +35,12 @@ def test_can_init_three_node_network():
 
 def test_can_init_arbitrary_large_network():
     nodes = [ChordNode(ChordKey(k, 1024)) for k in range(0, 1024, 8)]
-    bootstrap = min(nodes, key=lambda n: n.node_id)
 
+    bootstrap = min(nodes, key=lambda n: n.node_id)
     for node in nodes:
         node.initiate_join(bootstrap)
 
-    succ_ids = [n.successor.node_id for n in nodes]
-    pred_ids = [n.predecessor.node_id for n in nodes]
-    fingers = [[f.node_id for f in n.fingers] for n in nodes]
-
-    print(succ_ids)
-    print(pred_ids)
-
     exp_succs = nodes[1:] + [nodes[0]]
     exp_preds = [nodes[-1]] + nodes[:-1]
-
-    print(128 - sum([1 for n, p, s in zip(nodes, exp_preds, exp_succs)
-               if n.predecessor == p and n.successor == s]))
-
     assert [n.successor.node_id for n in nodes] == [n.node_id for n in exp_succs]
     assert [n.predecessor.node_id for n in nodes] == [n.node_id for n in exp_preds]
