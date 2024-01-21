77165029c64270e01d06f339dc6420f1d60d6b34
Marco Tröster <marco@troester-gmbh.de>
2024-01-21 23:17:08 +0100

    test transmission protocol between chord nodes (wip)

---
+++ tests/networked_join_test.py
@@ -0,0 +1,35 @@
+from typing import List
+from threading import Thread
+from chordlite import \
+    IPEndpointId, ChordNode, VirtualNetwork, \
+    NetworkedChordNode, ChordRemoteEndpoint
+
+
+def test_can_init_network_over_virtual_network():
+    network = VirtualNetwork()
+    nodes = [NetworkedChordNode(IPEndpointId(f"10.0.0.{key}", "5555", 1 << 14), network)
+             for key in range(128)]
+    nodes = sorted(nodes, key=lambda n: n.node_id)
+    assert len(set([n.node_id for n in nodes])) == 128
+
+    def exp_fingers_of_node(node: ChordNode) -> List[ChordNode]:
+        return [min(nodes, key=lambda f: f.node_id - s) for s in node.finger_starts]
+
+    for node in nodes:
+        network.register_node(node.server)
+
+    bootstrap_id = min([n.node_id for n in nodes])
+    bootstrap_endpoints = [ChordRemoteEndpoint(n.node_id, bootstrap_id, network) for n in nodes]
+    threads = [Thread(target=lambda: n.join_network(b))
+               for n, b in zip(nodes, bootstrap_endpoints)]
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+
+    exp_succs = nodes[1:] + [nodes[0]]
+    exp_preds = [nodes[-1]] + nodes[:-1]
+    exp_fingers = [f.node_id for n in nodes for f in exp_fingers_of_node(n.node)]
+    assert [n.node.successor.node_id for n in nodes] == [n.node_id for n in exp_succs]
+    assert [n.node.predecessor.node_id for n in nodes] == [n.node_id for n in exp_preds]
+    assert [f.node_id for n in nodes for f in n.node.fingers] == exp_fingers
